29e3b54beaf54b3536e7599df264c623e0f40303
Zoe Ames <zoeames@gmail.com>
2019-06-29 13:09:31 -0500

    moved axios calls to HOME

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,22 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import fishData from '../../helpers/data/fishData';
+import fishShapes from '../../helpers/propz/fishShapes';
+
 import Fish from '../Fish/Fish';
 import './Inventory.scss';
 
 class Inventory extends React.Component {
-  state = {
-    fishes: [],
-  }
-
-  componentDidMount() {
-    fishData.getFishes()
-      .then(fishes => this.setState({ fishes }))
-      .catch(err => console.error('could not get fishes', err));
+  static propTypes = {
+    fishes: PropTypes.arrayOf(fishShapes.fishShape),
   }
 
   render() {
-    const fishComponents = this.state.fishes.map(fish => (
+    const fishComponents = this.props.fishes.map(fish => (
       <Fish key={fish.id} fish={fish}/>
     ));
 
